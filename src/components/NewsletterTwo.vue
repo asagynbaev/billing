f3bd9607f7d7a960ff7648b46cd183d902723935
Azimbek Sagynbaev <sagynbaev6@gmail.com>
2024-02-23 00:07:37 +0600

    first commit

---
+++ src/components/NewsletterTwo.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <div class="section-title-wrapper text-center section-space--mb_50 wow move-up">
+                <h2 class="section-title font-width-bold">Подпишитесь на нашу рассылку</h2>
+            </div>
+            <!-- newsletter form -->
+            <div class="newsletter-form--two section-space--mb_40 text-center wow move-up">
+                <form>
+                    <input type="text" placeholder="Your name">
+                    <input type="email" placeholder="Email">
+                    <button class="ht-btn ht-btn-md">Подписаться!</button>
+                </form>
+            </div>
+            <!-- newsletter text -->
+             </div>
+    </div>
+</template>
---
+++ src/components/sections/HeaderPayment.vue
@@ -0,0 +1,72 @@
+<template>
+    <header class="header-area navbar-light text-white fixed-top" :class="{'is-sticky': isSticky}">
+        <div class="container-fluid container-fluid--cp-150">
+            <nav class="navbar navbar-expand-lg" id="navbar-example2">
+                <router-link to="/" class="navbar-brand logo-light">
+                    <img src="../../assets/img/logo/logo.svg" alt="logo">
+                </router-link>
+                <router-link to="/" class="navbar-brand logo-dark">
+                    <img src="../../assets/img/logo/logo-dark.png" alt="logo">
+                </router-link>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                    <ul class="nav main-menu">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#home" @click="menuCollapseOnLinkClick">
+                                <span>ГЛАВНАЯ</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#about" @click="menuCollapseOnLinkClick">
+                                <span>О НАС</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#service" @click="menuCollapseOnLinkClick">
+                                <span>СЕРВИСЫ</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#pricing" @click="menuCollapseOnLinkClick">
+                                <span>ЦЕНЫ</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#clients" @click="menuCollapseOnLinkClick">
+                                <span>КЛИЕНТЫ</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+    export default {
+        data (){
+            return {
+                isSticky: false
+            }
+        },
+        mounted(){
+            window.addEventListener('scroll', () => {
+                let scroll = window.scrollY
+                if(scroll >= 200){
+                    this.isSticky = true
+                } else {
+                    this.isSticky = false
+                }
+            })
+        },
+        methods: {
+            menuCollapseOnLinkClick() {
+                document.getElementById('navbarSupportedContent').classList.remove('show')
+            }
+        },
+    }
+</script>
